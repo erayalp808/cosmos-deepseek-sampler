f40d59f3e143a0c7580e8b4f59c7cdc75df1c79a
erayalp808 <erayalp.e@gmail.com>
2025-02-07 18:24:45 +0300

    feat(file_read): add csv file writing for each response sample

--- deepseek-sampler.py
+++ deepseek-sampler.py
@@ -2,9 +2,12 @@ import pandas as pd
 import numpy as np
 from helpers import *
 
+verbose = True
 instructions = pd.read_csv('instructions.csv')
+output_file_path = 'turkish_instructions.csv'
+output_df = get_output_df(output_file_path)
 
-for row in instructions[:3].itertuples():
+for row in instructions[:10].itertuples():
     index, _, prompt, input_values, output = row
     input_values = '' if pd.isna(input_values) else input_values
     prompt_final = prompt + input_values
@@ -14,7 +17,21 @@ for row in instructions[:3].itertuples():
     if response.status_code == 200:
         response = response.json()
         generated_output = response['choices'][0]['message']['content']
-        parse_reasoned_output(generated_output)
-    else:
-        print(f"Error: {response.status_code}, {response.text}")
+        thought_process, real_output = parse_reasoned_output(generated_output)
+        sample = [
+            index,
+            prompt,
+            input_values,
+            thought_process,
+            real_output
+        ]
+        output_df.loc[len(output_df)] = sample
 
+        if verbose:
+            print(index, prompt, input_values)
+            print('THINK_START', thought_process, 'THINK_END')
+            print('OUTPUT_START', real_output, 'OUTPUT_END')
+    else:
+        print(f"Error: { response.status_code }, { response.text }")
+
+output_df.to_csv(output_file_path)
