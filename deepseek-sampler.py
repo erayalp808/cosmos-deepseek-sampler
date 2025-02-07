0d9287e350ed7ce91e4851bf227f265a28921c60
erayalp808 <erayalp.e@gmail.com>
2025-02-07 17:09:29 +0300

    feat(file_read): add csv data iteration to make api calls on instructions

--- deepseek-sampler.py
+++ deepseek-sampler.py
@@ -1,46 +1,20 @@
-import requests
-import re
-from dotenv import load_dotenv
-import os
+import pandas as pd
+import numpy as np
+from helpers import *
 
-load_dotenv()
+instructions = pd.read_csv('instructions.csv')
 
-url = "https://api.groq.com/openai/v1/chat/completions"
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": f"Bearer { os.getenv('API_KEY') }"
-}
-data = {
-    "model": "deepseek-r1-distill-llama-70b",
-    "messages": [
-        {
-            "role": "user",
-            "content": "what are the prompts that make you think the most"
-        }
-    ]
-}
+for row in instructions[:3].itertuples():
+    index, _, prompt, input_values, output = row
+    input_values = '' if pd.isna(input_values) else input_values
+    prompt_final = prompt + input_values
 
-response = requests.post(url, headers=headers, json=data)
+    response = get_deepseek_response(prompt_final)
 
-if response.status_code == 200:
-    response = response.json()
-    output_content = response['choices'][0]['message']['content']
-    think_tag_pattern = r'<think>(.*?)</think>'
-    think_elements = re.findall(think_tag_pattern, output_content, re.DOTALL)
-
-    print('THINK_START')
-    for think_element in think_elements:
-        print(think_element)
-    print('THINK_END')
-
-    print('OUTPUT_START')
-    last_think_end = output_content.rfind('</think>')
-
-    if last_think_end != -1:
-        content_after_last_think = output_content[last_think_end + len('</think>'):].strip()
-        print(content_after_last_think)
+    if response.status_code == 200:
+        response = response.json()
+        generated_output = response['choices'][0]['message']['content']
+        parse_reasoned_output(generated_output)
     else:
-        print('No closing </think> tag found.')
-    print('OUTPUT_END')
-else:
-    print(f"Error: {response.status_code}, {response.text}")
+        print(f"Error: {response.status_code}, {response.text}")
+
